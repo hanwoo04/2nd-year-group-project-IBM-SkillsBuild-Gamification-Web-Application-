d56a5fd8ff7d29658470eaf688474c61a12d9c98
benmill97 <ben.mill.97@gmail.com>
2024-01-31 11:35:43 +0000

    initial project creation

---
+++ ibm-skillsbuild-app/src/main/java/org/example/ibmskillsbuildapp/IbmSkillsbuildAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.ibmskillsbuildapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmSkillsbuildAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbmSkillsbuildAppApplication.class, args);
+    }
+
+}
