17521e5bde920759d38e127cd57d0b70ee2e1d18
Enzo Brown <eb433@student.le.ac.uk>
2024-02-08 12:28:14 +0000

    "Initial Creation of Leaderboard, currently only displays global standing"

--- ibm-skillsbuild-app/src/main/java/org/example/ibmskillsbuildapp/IbmSkillsbuildAppApplication.java
+++ ibm-skillsbuild-app/src/main/java/org/example/ibmskillsbuildapp/IbmSkillsbuildAppApplication.java
@@ -1,13 +1,39 @@
 package org.example.ibmskillsbuildapp;
 
+import org.example.ibmskillsbuildapp.model.Player;
+import org.example.ibmskillsbuildapp.repo.LeaderboardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IbmSkillsbuildAppApplication {
-
+public class IbmSkillsbuildAppApplication implements CommandLineRunner {
+    @Autowired
+    private LeaderboardRepository repo;
     public static void main(String[] args) {
         SpringApplication.run(IbmSkillsbuildAppApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        Player player = new Player();
+        player.setName("Joe");
+        player.setScore(500);
+        player = repo.save(player);
+
+        Player player2 = new Player();
+        player2.setName("Jack");
+        player2.setScore(200);
+        player2 = repo.save(player2);
+
+        Player player3 = new Player();
+        player3.setName("Wil");
+        player3.setScore(1000);
+        player3 = repo.save(player3);
+
+
+
+    }
 }
