65504678619c21a335a7b5111ab89e06a63c8fcc
Kevin Han <kevinjungwoohan@gmail.com>
2024-02-19 10:28:18 +0000

    .

--- ibm-skillsbuild-app/src/main/java/org/example/ibmskillsbuildapp/IbmSkillsbuildAppApplication.java
+++ ibm-skillsbuild-app/src/main/java/org/example/ibmskillsbuildapp/IbmSkillsbuildAppApplication.java
@@ -1,18 +1,24 @@
 package org.example.ibmskillsbuildapp;
 
+import org.example.ibmskillsbuildapp.model.User;
+import org.example.ibmskillsbuildapp.model.UserRoles;
+import org.example.ibmskillsbuildapp.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
-public class IbmSkillsbuildAppApplication implements CommandLineRunner {
-    public static void main(String[] args) {
-        SpringApplication.run(IbmSkillsbuildAppApplication.class, args);
-    }
+public class IbmSkillsBuildAppApplication implements CommandLineRunner {
 
+    public static void main(String[] args) {
+        SpringApplication.run(IbmSkillsBuildAppApplication.class, args);
+    }
 
     @Override
     public void run(String... args) throws Exception {
+
     }
 }
