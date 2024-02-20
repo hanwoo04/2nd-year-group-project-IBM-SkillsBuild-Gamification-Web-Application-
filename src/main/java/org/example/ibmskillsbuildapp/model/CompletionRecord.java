884bf411bdc2689f75f9ffd359c5bb79cdc55519
leandrodenoronha <ldn5@student.le.ac.uk>
2024-02-20 18:04:24 +0000

    CompletionRecord class

---
+++ src/main/java/org/example/ibmskillsbuildapp/model/CompletionRecord.java
@@ -0,0 +1,30 @@
+package org.example.ibmskillsbuildapp.model;
+
+public class CompletionRecord {
+
+    private String taskName;
+    private String completionDate;
+    private boolean isSuccess;
+
+
+    public CompletionRecord(String taskName, String completionDate, boolean isSuccess) {
+        this.taskName = taskName;
+        this.completionDate = completionDate;
+        this.isSuccess = isSuccess;
+    }
+
+    public void displayRecord() {
+        System.out.println("Task Name: " + taskName);
+        System.out.println("Completion Date: " + completionDate);
+        System.out.println("Success: " + isSuccess);
+    }
+
+
+    public static void main(String[] args) {
+
+        CompletionRecord record = new CompletionRecord("Sample Task", "2024-02-13", true);
+
+
+        record.displayRecord();
+    }
+}
