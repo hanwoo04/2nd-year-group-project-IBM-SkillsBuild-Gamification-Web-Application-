ce71511d6e827e307f1c4fd10904a688eda9bd7a
leandrodenoronha <ldn5@student.le.ac.uk>
2024-02-20 20:15:56 +0000

    Add CompletionRecord class

---
+++ src/main/java/org/example/ibmskillsbuildapp/LoadDatabase.java
@@ -0,0 +1,59 @@
+package org.example.ibmskillsbuildapp;
+
+import org.example.ibmskillsbuildapp.service.CourseService;
+import org.example.ibmskillsbuildapp.service.LearningPathService;
+import org.example.ibmskillsbuildapp.service.UserService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class for loading initial data into the database. This class checks if the
+ * repositories for users, courses, and learning paths are empty, and if so, creates and saves new
+ * entities.
+ */
+@Configuration
+public class LoadDatabase {
+
+    private final UserService userService;
+    private final CourseService courseService;
+    private final LearningPathService learningPathService;
+
+    /**
+     * Constructs a new LoadDatabase object with the specified user, course, and learning path
+     * services.
+     *
+     * @param userService         the service for managing users.
+     * @param courseService       the service for managing courses.
+     * @param learningPathService the service for managing learning paths.
+     */
+    public LoadDatabase(UserService userService, CourseService courseService,
+        LearningPathService learningPathService) {
+        this.userService = userService;
+        this.courseService = courseService;
+        this.learningPathService = learningPathService;
+    }
+
+    /**
+     * Initializes the database with users, courses, and learning paths if their respective
+     * repositories are empty.
+     *
+     * @return a CommandLineRunner that runs the initialization logic.
+     */
+    @Bean
+    CommandLineRunner initDatabase() {
+        return args -> {
+            if (learningPathService.isEmpty()) {
+                learningPathService.createLearningPaths();
+            }
+
+            if (courseService.isEmpty()) {
+                courseService.createCourses();
+            }
+
+            if (userService.isEmpty()) {
+                userService.createUsers();
+            }
+        };
+    }
+}
---
+++ src/test/java/org/example/ibmskillsbuildapp/model/CourseViewTest.java
@@ -0,0 +1,60 @@
+package org.example.ibmskillsbuildapp.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class CourseViewTest {
+
+    private CourseView courseView;
+
+    @BeforeEach
+    public void setup() {
+        courseView = new CourseView("pathName", "courseName", "description", LearningStatus.STARTED, "url");
+    }
+
+    @Test
+    void testPathName() {
+        String pathName = "Test Path";
+        courseView.setPathName(pathName);
+        assertEquals(pathName, courseView.getPathName());
+    }
+
+    @Test
+    void testCourseName() {
+        String courseName = "Test Course";
+        courseView.setCourseName(courseName);
+        assertEquals(courseName, courseView.getCourseName());
+    }
+
+    @Test
+    void testStatus() {
+        LearningStatus status = LearningStatus.COMPLETED;
+        courseView.setStatus(status);
+        assertEquals(status, courseView.getStatus());
+    }
+
+    @Test
+    void testUrl() {
+        String url = "http://testcourse.com";
+        courseView.setUrl(url);
+        assertEquals(url, courseView.getUrl());
+    }
+
+    @Test
+    void testConstructor() {
+        String pathName = "Test Path";
+        String courseName = "Test Course";
+        String description = "This is a test course";
+        LearningStatus status = LearningStatus.COMPLETED;
+        String url = "http://testcourse.com";
+        CourseView courseView = new CourseView(pathName, courseName, description, status, url);
+        assertNotNull(courseView);
+        assertEquals(pathName, courseView.getPathName());
+        assertEquals(courseName, courseView.getCourseName());
+        assertEquals(status, courseView.getStatus());
+        assertEquals(url, courseView.getUrl());
+    }
+}
