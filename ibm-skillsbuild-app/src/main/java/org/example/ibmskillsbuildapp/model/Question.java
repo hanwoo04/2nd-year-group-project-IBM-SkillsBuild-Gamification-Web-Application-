d8603fb46445d1972ade10d25fc4b37925685aa8
L De Noronha <ldn5@student.le.ac.uk>
2024-03-21 19:34:56 +0000

    Removed QuizRepo. Implemented hardcoded questions into QuizService

--- ibm-skillsbuild-app/src/main/java/org/example/ibmskillsbuildapp/model/Question.java
+++ ibm-skillsbuild-app/src/main/java/org/example/ibmskillsbuildapp/model/Question.java
@@ -1,8 +1,6 @@
 package org.example.ibmskillsbuildapp.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -11,88 +9,26 @@ import jakarta.persistence.Table;
 public class Question {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int quesId;
     private String title;
     private String optionA;
     private String optionB;
     private String optionC;
     private int ans;
-    private int chose;
 
     public Question() {
-        super();
+
     }
 
-    public Question(int quesId, String title, String optionA, String optionB, String optionC, int ans, int chose) {
-        super();
+
+    public Question(int quesId, String title, String optionA, String optionB, String optionC, int ans) {
         this.quesId = quesId;
         this.title = title;
         this.optionA = optionA;
         this.optionB = optionB;
         this.optionC = optionC;
         this.ans = ans;
-        this.chose = chose;
     }
 
-    public int getQuesId() {
-        return quesId;
-    }
 
-    public void setQuesId(int quesId) {
-        this.quesId = quesId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getOptionA() {
-        return optionA;
-    }
-
-    public void setOptionA(String optionA) {
-        this.optionA = optionA;
-    }
-
-    public String getOptionB() {
-        return optionB;
-    }
-
-    public void setOptionB(String optionB) {
-        this.optionB = optionB;
-    }
-
-    public String getOptionC() {
-        return optionC;
-    }
-
-    public void setOptionC(String optionC) {
-        this.optionC = optionC;
-    }
-
-    public int getAns() {
-        return ans;
-    }
-
-    public void setAns(int ans) {
-        this.ans = ans;
-    }
-
-    public int getChose() {
-        return chose;
-    }
-
-    public void setChose(int chose) {
-        this.chose = chose;
-    }
-
-    @Override
-    public String toString() {
-        return "Question [quesId=" + quesId + ", title=" + title + ", optionA=" + optionA + ", optionB=" + optionB + ", optionC=" + optionC + ", ans=" + ans + ", chose=" + chose + "]";
-    }
 }
--- ibm-skillsbuild-app/src/main/java/org/example/ibmskillsbuildapp/service/QuizService.java
+++ ibm-skillsbuild-app/src/main/java/org/example/ibmskillsbuildapp/service/QuizService.java
@@ -3,7 +3,6 @@ package org.example.ibmskillsbuildapp.service;
 import org.example.ibmskillsbuildapp.model.Question;
 import org.example.ibmskillsbuildapp.model.QuestionForm;
 import org.example.ibmskillsbuildapp.model.Result;
-import org.example.ibmskillsbuildapp.repo.QuestionRepo;
 import org.example.ibmskillsbuildapp.repo.ResultRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
@@ -11,42 +10,43 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 @Service
 public class QuizService {
 
-    @Autowired
-    private QuestionRepo questionRepo;
-    @Autowired
     private ResultRepo resultRepo;
 
+    @Autowired
+    public QuizService(ResultRepo resultRepo) {
+        this.resultRepo = resultRepo;
+    }
+
+    private static final List<Question> hardcodedQuestions = createHardcodedQuestions();
+
+    private static List<Question> createHardcodedQuestions() {
+        List<Question> questions = new ArrayList<>();
+        questions.add(new Question(1, "Which framework is used for building enterprise applications in Java?",
+                "Spring Framework", "ReactJS", "Vue.js", 1)); // Option A is correct
+        questions.add(new Question(2, "IBM Watson SDK for Java is used to integrate with what type of IBM services",
+                "1989", "Artificial intelligence services", "2000", 2)); // Option B is correct
+        questions.add(new Question(3, "What is the principle of writing reusable code components in Java?",
+                "Cloud storage", "Artificial intelligence services", "Blockchain services", 3)); // Option C is correct
+        questions.add(new Question(4, "Which tool is preferred for continuous integration/continuous delivery (CI/CD) in Java projects, often used in IBM tutorials?",
+                "Jenkins", "Travis CI", "GitLab CI", 1)); // Option A is correct
+        return questions;
+    }
+
     public QuestionForm getQuestions() {
-        List<Question> allQuestions = questionRepo.findAll();
-        List<Question> questionList = new ArrayList<>();
-        Random random = new Random();
-
-        for(int i = 0; i < 5; i++) {
-            int rand = random.nextInt(allQuestions.size());
-            questionList.add(allQuestions.get(rand));
-            allQuestions.remove(rand);
-        }
-
         QuestionForm questionForm = new QuestionForm();
-        questionForm.setQuestions(questionList);
-
+        questionForm.setQuestions(new ArrayList<>(hardcodedQuestions));
         return questionForm;
     }
 
     public int getResult(QuestionForm questionForm) {
         int correctAnswers = 0;
+        for (Question question : questionForm.getQuestions()) {
 
-        for(Question question: questionForm.getQuestions()) {
-            if(question.getAns() == question.getChose()) {
-                correctAnswers++;
-            }
         }
-
         return correctAnswers;
     }
 
